94e5b484d40fc9fe5a79fbd42458e5669cac837a
Nicosoft <nicolas.jourdain@gmail.com>
2019-03-26 13:27:19 +1100

    Limit the number of characters

--- classes/output/core/course_renderer.php
+++ classes/output/core/course_renderer.php
@@ -64,9 +64,36 @@ class course_renderer extends \core_course_renderer {
      * @return string html for the page
      */
     public function render_activity_navigation(\core_course\output\activity_navigation $page) {
+        global $OUTPUT;
+
         $data = $page->export_for_template($this->output);
-        $data->prevlink->classes = "btn btn-default";
-        $data->nextlink->classes = "btn btn-default";
+
+        $textlength = 20;
+        
+        if (isset($data->prevlink)) {
+            // Add button style for the link.
+            $data->prevlink->classes = "btn btn-primary";
+            // Limit the number of characters for the link text.
+            // Limit the number of characters for the link text.
+            foreach ($data->prevlink->attributes as $key => $attribute) {
+                if ($attribute['name'] === 'title') {
+                    // Build new text and add text-icon at the beginning.
+                    $data->prevlink->text = $OUTPUT->larrow() . ' ' . substr($attribute['value'], 0, $textlength);
+                }
+            }
+        }
+
+        if (isset($data->nextlink)) {
+            // Add button style for the link.
+            $data->nextlink->classes = "btn btn-primary";
+            // Limit the number of characters for the link text.
+            foreach ($data->nextlink->attributes as $key => $attribute) {
+                if ($attribute['name'] === 'title') {
+                    // Build new text and add text-icon at the end.
+                    $data->nextlink->text = substr($attribute['value'], 0, $textlength) . ' ' . $OUTPUT->rarrow();
+                }
+            }
+        }
         return $this->output->render_from_template('core_course/activity_navigation', $data);
     }
 
@@ -251,5 +278,92 @@ class course_renderer extends \core_course_renderer {
             }
         }
         return $output;
+    }
+
+
+    /**
+     * Output the html for a single section page .
+     *
+     * @param stdClass $course The course entry from DB
+     * @param array $sections (argument not used)
+     * @param array $mods (argument not used)
+     * @param array $modnames (argument not used)
+     * @param array $modnamesused (argument not used)
+     * @param int $displaysection The section number in the course which is being displayed
+     */
+    public function print_single_section_page($course, $sections, $mods, $modnames, $modnamesused, $displaysection) {
+        global $PAGE;
+
+        $modinfo = get_fast_modinfo($course);
+        $course = course_get_format($course)->get_course();
+
+        // Can we view the section in question?
+        if (!($sectioninfo = $modinfo->get_section_info($displaysection)) || !$sectioninfo->uservisible) {
+            // This section doesn't exist or is not available for the user.
+            // We actually already check this in course/view.php but just in case exit from this function as well.
+            print_error('unknowncoursesection', 'error', course_get_url($course),
+                format_string($course->fullname));
+        }
+
+        // Copy activity clipboard..
+        echo $this->course_activity_clipboard($course, $displaysection);
+        $thissection = $modinfo->get_section_info(0);
+        if ($thissection->summary or !empty($modinfo->sections[0]) or $PAGE->user_is_editing()) {
+            echo $this->start_section_list();
+            echo $this->section_header($thissection, $course, true, $displaysection);
+            echo $this->courserenderer->course_section_cm_list($course, $thissection, $displaysection);
+            echo $this->courserenderer->course_section_add_cm_control($course, 0, $displaysection);
+            echo $this->section_footer();
+            echo $this->end_section_list();
+        }
+
+        // Start single-section div
+        echo html_writer::start_tag('div', array('class' => 'single-section'));
+
+        // The requested section page.
+        $thissection = $modinfo->get_section_info($displaysection);
+
+        // Title with section navigation links.
+        $sectionnavlinks = $this->get_nav_links($course, $modinfo->get_section_info_all(), $displaysection);
+        $sectiontitle = '';
+        $sectiontitle .= html_writer::start_tag('div', array('class' => 'section-navigation navigationtitle'));
+        $sectiontitle .= html_writer::tag('span', $sectionnavlinks['previous'], array('class' => 'mdl-left'));
+        $sectiontitle .= html_writer::tag('span', $sectionnavlinks['next'], array('class' => 'mdl-right'));
+        // Title attributes
+        $classes = 'sectionname';
+        if (!$thissection->visible) {
+            $classes .= ' dimmed_text';
+        }
+        $sectionname = html_writer::tag('span', $this->section_title_without_link($thissection, $course));
+        $sectiontitle .= $this->output->heading($sectionname, 3, $classes);
+
+        $sectiontitle .= html_writer::end_tag('div');
+        echo $sectiontitle;
+
+        // Now the list of sections..
+        echo $this->start_section_list();
+
+        echo $this->section_header($thissection, $course, true, $displaysection);
+        // Show completion help icon.
+        $completioninfo = new completion_info($course);
+        echo $completioninfo->display_help_icon();
+
+        echo $this->courserenderer->course_section_cm_list($course, $thissection, $displaysection);
+        echo $this->courserenderer->course_section_add_cm_control($course, $displaysection, $displaysection);
+        echo $this->section_footer();
+        echo $this->end_section_list();
+
+        // Display section bottom navigation.
+        $sectionbottomnav = '';
+        $sectionbottomnav .= html_writer::start_tag('div', array('class' => 'section-navigation mdl-bottom nico-111'));
+        $sectionbottomnav .= html_writer::tag('span', $sectionnavlinks['previous'], array('class' => 'mdl-left'));
+        $sectionbottomnav .= html_writer::tag('span', $sectionnavlinks['next'], array('class' => 'mdl-right'));
+        $sectionbottomnav .= html_writer::tag('div', $this->section_nav_selection($course, $sections, $displaysection),
+            array('class' => 'mdl-align'));
+        $sectionbottomnav .= html_writer::end_tag('div');
+        echo $sectionbottomnav;
+
+        // Close single-section div.
+        echo html_writer::end_tag('div');
     }
 }
