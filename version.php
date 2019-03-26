abb033a620aedc5ad56911806007c7d0175414f6
Nicosoft <nicolas.jourdain@gmail.com>
2019-03-26 13:28:20 +1100

    Update version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2019032201;
+$plugin->version = 2019032600;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
