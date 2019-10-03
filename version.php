73cfaeae5386fe6ec105762fcd59201ded6eb18d
Nicosoft <nicolas.jourdain@gmail.com>
2019-10-03 10:53:18 +1000

    Bump version

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 // This is the version of the plugin.
-$plugin->version = 2019040402;
+$plugin->version = 2019100300;
 
 // This is the version of Moodle this plugin requires.
 $plugin->requires = 2018050800;
@@ -44,5 +44,5 @@ $plugin->dependencies = [
 $plugin->maturity = MATURITY_STABLE;
 
 // This is the named version.
-$plugin->release = 1.5;
+$plugin->release = 1.6;
 
